6878fbe29537b8fdebe4874418c281e4e08416c3
Jordan Santos <jordansant@gmail.com>
2022-11-29 22:16:23 -0300

    Criação do projeto

---
+++ src/main/java/com/jordansantos/tqi/ProjetoTqiApplication.java
@@ -0,0 +1,13 @@
+package com.jordansantos.tqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTqiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jordansantos/tqi/ProjetoTqiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordansantos.tqi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTqiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
